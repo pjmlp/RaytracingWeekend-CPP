7989ca23f815991875a529a94d8a1a803c9cad65
pjmlp <pjmlp@progtools.org>
2022-01-05 21:34:09 +0100

    Adding the chapter 6.2.

--- OneWeekend/Raytracing/main.cpp
+++ OneWeekend/Raytracing/main.cpp
@@ -9,15 +9,16 @@ using namespace Raytracinglib::images;
 
 double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+
     if (discriminant < 0) {
         return -1.0;
     }
     else {
-        return (-b - sqrt(discriminant)) / (2.0 * a);
+        return (-half_b - sqrt(discriminant)) / a;
     }
 }
 
