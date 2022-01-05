fd378ed275fedb4eb8168d5bb13cc83d0780bc49
pjmlp <pjmlp@progtools.org>
2022-01-05 21:31:43 +0100

    Adding the chapter 5.2.

--- OneWeekend/Raytracing/main.cpp
+++ OneWeekend/Raytracing/main.cpp
@@ -7,13 +7,25 @@ import images;
 
 using namespace Raytracinglib::images;
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main() {
+
     // Image
     constexpr auto aspect_ratio = 16.0 / 9.0;
     constexpr int image_width = 400;
