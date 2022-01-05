8acda16e7b2f6c2b33487ef5a6054c8ab132a4fa
pjmlp <pjmlp@progtools.org>
2022-01-05 21:31:03 +0100

    Adding the chapter 4.2.

--- OneWeekend/Raytracing/main.cpp
+++ OneWeekend/Raytracing/main.cpp
@@ -1,24 +1,44 @@
 import <iostream>;
 
-import color;
 import algebra;
+import color;
+import ray;
 import images;
 
 using namespace Raytracinglib::images;
 
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main() {
     // Image
-    constexpr int image_width = 256;
-    constexpr int image_height = 256;
+    constexpr auto aspect_ratio = 16.0 / 9.0;
+    constexpr int image_width = 400;
+    constexpr int image_height = static_cast<int>(image_width / aspect_ratio);
 
     ImageBuffer buffer(image_width, image_height, PixelFormat::RGB);
 
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     // Render
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
             buffer.set_color(i, j, pixel_color);
         }
     }
