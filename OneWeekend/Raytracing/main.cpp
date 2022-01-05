a79039a1c617c1177a1b8e530ece152e5a0631e0
pjmlp <pjmlp@progtools.org>
2022-01-05 21:30:24 +0100

    Adding the chapter 3.3.

--- OneWeekend/Raytracing/main.cpp
+++ OneWeekend/Raytracing/main.cpp
@@ -1,6 +1,7 @@
-
 import <iostream>;
 
+import color;
+import algebra;
 import images;
 
 using namespace Raytracinglib::images;
@@ -12,19 +13,13 @@ int main() {
 
     ImageBuffer buffer(image_width, image_height, PixelFormat::RGB);
 
+
     // Render
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            auto ir = static_cast<std::uint8_t>(255.999 * r);
-            auto ig = static_cast<std::uint8_t>(255.999 * g);
-            auto ib = static_cast<std::uint8_t>(255.999 * b);
-
-            buffer.set_color(i, j, ir, ig, ib);
+            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            buffer.set_color(i, j, pixel_color);
         }
     }
 
