159f0315f1c88b21beb8b0134af30d3b7a579bed
Paulo Pinto <paulo.pinto@ecx.io>
2022-02-09 16:37:08 +0100

    Made it parallel with concurrency::parallel_for.

--- OneWeekend/Raytracing/main.cpp
+++ OneWeekend/Raytracing/main.cpp
@@ -1,3 +1,6 @@
+#include <windows.h>
+#include <ppl.h>
+
 import <iostream>;
 import <memory>;
 import <vector>;
@@ -52,19 +55,22 @@ int main() {
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     // Render
-    for (int j = image_height - 1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width - 1);
-                auto v = (j + random_double()) / (image_height - 1);
-                ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
+    // Improve this to be more portable, right now just POC
+    concurrency::parallel_for(size_t(0), size_t(image_height), [&](size_t index)
+        {
+            int j = image_height - index - 1;
+            std::cerr << "Processing scanline: " << index << '\n' << std::flush;
+            for (int i = 0; i < image_width; ++i) {
+                color pixel_color(0, 0, 0);
+                for (int s = 0; s < samples_per_pixel; ++s) {
+                    auto u = (i + random_double()) / (image_width - 1);
+                    auto v = (j + random_double()) / (image_height - 1);
+                    ray r = cam.get_ray(u, v);
+                    pixel_color += ray_color(r, world, max_depth);
+                }
+                buffer.set_color(i, j, pixel_color, samples_per_pixel);
             }
-            buffer.set_color(i, j, pixel_color, samples_per_pixel);
-        }
-    }
+        });
 
     buffer.save("img.png");
     std::cerr << "\nDone.\n";
